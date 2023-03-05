ae102a95dbe476c972e68114dec2f141f978e84c
Srinivas Elimilla <114722049+elimillasrinivas@users.noreply.github.com>
2023-03-05 14:46:33 +0530

    Update index.js
    Changed in index secure and reactURL

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const { getAuthenticate } = require("./authentication/authentication");
 const conn=require("./connection/connect");
 conn();//connection with backend established
 dotenv.config();
-const reactUrl = process.env.REACT_URL || "https://magenta-paletas-9f0a10.netlify.app"
+const reactUrl = "https://magenta-paletas-9f0a10.netlify.app"
 
 const app = express();
 app.use(cors({
