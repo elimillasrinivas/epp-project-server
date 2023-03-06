c0c96803df4862e16e25f1033b86c0daae98ba70
Srinivas Elimilla <114722049+elimillasrinivas@users.noreply.github.com>
2023-03-06 14:46:21 +0530

    Update index.js
    back to previous link as i forgot to change localhost to render while build

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const { getAuthenticate } = require("./authentication/authentication");
 const conn=require("./connection/connect");
 conn();//connection with backend established
 dotenv.config();
-const reactUrl = "https://glistening-panda-bc6259.netlify.app"
+const reactUrl = "https://epp-project-grp2-10x.netlify.app"
 
 const app = express();
 app.use(cors({
